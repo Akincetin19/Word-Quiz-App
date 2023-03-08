3eb3e939ef92c6b81144b583eb70e1d153f5b655
Akincetin19 <119444077+Akincetin19@users.noreply.github.com>
2023-03-08 03:26:02 +0300

    Initial Commit

---
+++ Word-Quiz-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Word-Quiz-App
+//
+//  Created by Akın Çetin on 8.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
